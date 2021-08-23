e8df6642fbccc4448b0708942922db696bc350fd
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2021-08-23 12:58:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Main from "./Components/Main";
+
+function App() {
+
+  return (<>
+    <h1>MY EEXPENSES </h1>
+    <Main />
+  </>
+  );
+}
+
+export default App;
