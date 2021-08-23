f1a60e5102f480231d483b643cb799953d85aef6
rashidali8210 <71310260+rashidali8210@users.noreply.github.com>
2021-08-23 12:55:04 +0530

    Create index.js

---
+++ src/Redux/Action/index.js
@@ -0,0 +1,40 @@
+
+
+export const temp = () => {
+    return {
+        type: "TEMP",
+        id: 344
+    }
+}
+
+export const remove = () => {
+    return {
+        type: "REMOVE",
+        payload: 890
+    }
+}
+export const
+    setInputTitle= (title) => ({
+      type: "SET_INPUT_TITLE",
+      title,
+    }) 
+    export const
+    setInputAmount= (amount) => ({
+      type: "SET_INPUT_AMOUNT",
+      amount,
+    }) 
+    export const
+    setInputNote= (note) => ({
+      type: "SET_INPUT_NOTE",
+      note,
+    }) 
+    export const
+    setInputDate= (date) => ({
+      type: "SET_INPUT_DATE",
+      date,
+    }) 
+    export const
+    setReset= () => ({
+      type: "RESET_INPUT",
+      
+    }) 
